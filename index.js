ff0db0daf42f59fdfe29f1dd7fa8e30c74326351
elizabeth padron <elizabethpadron02@gmail.com>
2024-03-03 14:45:15 -0600

    added answers function to index.js file

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 
+const inquirer = require('inquirer');
+const { Square, Text } = require('./lib/shapes.');
+const fs = require('fs');
+
+async function promptUser() {
+  const answers = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Select a shape:',
+      choices: ['Square', 'Text'],
+    },
+    {
+      type: 'input',
+      name: 'size',
+      message: 'Enter the size:',
+      when: (answers) => answers.shape === 'Square',
+      validate: (value) => !isNaN(value) && value > 0,
+    },
+    {
+      type: 'input',
+      name: 'color',
+      message: 'Enter the color:',
+    },
+    {
+      type: 'input',
+      name: 'content',
+      message: 'Enter the text content:',
+      when: (answers) => answers.shape === 'Text',
+    },
+  ]);
+
+  return answers;
+}
 
