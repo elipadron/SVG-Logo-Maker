60337ed4ba322de46852b2dd267c6afbcd262968
elizabeth padron <elizabethpadron02@gmail.com>
2024-03-03 14:38:58 -0600

    updated shapes.js file

---
+++ Lib/shapes.js
@@ -0,0 +1,21 @@
+
+class Square {
+    constructor(size, color) {
+      this.size = size;
+      this.color = color;
+    }
+  }
+  
+  class Text {
+    constructor(content, fontSize, color) {
+      this.content = content;
+      this.fontSize = fontSize;
+      this.color = color;
+    }
+  }
+  
+  module.exports = {
+    Square,
+    Text,
+  };
+  
