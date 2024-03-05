55183f0870565cda93440efef2532bc0c152b8b5
elizabeth padron <elizabethpadron02@gmail.com>
2024-03-04 18:01:34 -0600

    rearranged index.js

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # SVG-Logo-Maker
 
 ## Description 
+Node.js command-line application that takes in user input to generate a logo and save it as an SVG file. 
 
 ## Walkthrough Video
--- index.js
+++ index.js
@@ -4,33 +4,11 @@ const fs = require('fs');
 
 async function main() {
   const userInput = await promptUser();
-
-  let svgMarkup;
-  let size;
-
-  switch (userInput.shape) {
-    case 'Square':
-      size = 100;
-      const square = new Square(userInput.color);
-      svgMarkup = `<rect width="${size}" height="${size}" fill="${square.color}" />`;
-      break;
-    case 'Text':
-      const text = new Text(userInput.content, userInput.textColor);
-      svgMarkup = `<text x="10" y="40" font-size="12" fill="${text.textColor}">${text.content}</text>`;
-      break;
-    case 'Circle':
-      size = 160;
-      const circle = new Circle(userInput.color);
-      svgMarkup = `<circle cx="${size / 2}" cy="${size / 2}" r="${size / 2 - 10}" fill="${circle.color}" />`;
-      break;
-    default:
-      console.error('Invalid shape selected.');
-      return;
-  }
+  const svgMarkup = generateSvgMarkup(userInput);
 
   const svgContent = `
-    <svg xmlns="http://www.w3.org/2000/svg" width="${size}" height="${size}">
-      ${svgMarkup}
+    <svg xmlns="http://www.w3.org/2000/svg" width="${svgMarkup.size}" height="${svgMarkup.size}">
+      ${svgMarkup.content}
     </svg>
   `;
 
@@ -70,7 +48,35 @@ async function promptUser() {
   return { ...shapeAndColorAnswers, ...textContentAnswers };
 }
 
+function generateSvgMarkup(userInput) {
+  let size;
+  let content;
+
+  switch (userInput.shape) {
+    case 'Square':
+      size = 100;
+      const square = new Square(userInput.color);
+      content = `<rect width="${size}" height="${size}" fill="${square.color}" />`;
+      break;
+    case 'Text':
+      const text = new Text(userInput.content, userInput.textColor);
+      content = `<text x="10" y="40" font-size="16" fill="${text.textColor}">${text.content}</text>`;
+      break;
+    case 'Circle':
+      size = 160;
+      const circle = new Circle(userInput.color);
+      content = `<circle cx="${size / 2}" cy="${size / 2}" r="${size / 2 - 10}" fill="${circle.color}" />`;
+      break;
+    default:
+      console.error('Invalid shape selected.');
+      return;
+  }
+
+  return { size, content };
+}
+
 main();
+
 
 
 
